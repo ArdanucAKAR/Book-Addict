2f71a599063e7e65f12781ef6e12ec475be50fd7
Ardanuc AKAR <akarardanuc@gmail.com>
2019-12-03 20:15:44 +0300

    Anasayfa Tasarımına Başlandı

--- Book Addict/Book Addict/Controllers/HomeController.cs
+++ Book Addict/Book Addict/Controllers/HomeController.cs
@@ -6,33 +6,33 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
-using Book_Addict.Models;
 
 namespace Book_Addict.Controllers
 {
     public class HomeController : Controller
     {
-        string Baseurl = "https://book-addict-api.herokuapp.com/";
+        //string Baseurl = "https://book-addict-api.herokuapp.com/";
         public ActionResult Index()
         {
-            user EmpInfo = new user();
+            //user EmpInfo = new user();
 
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(Baseurl);
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri(Baseurl);
 
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //    client.DefaultRequestHeaders.Clear();
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                HttpResponseMessage Res = client.GetAsync("api/v1/users/1").Result;
+            //    HttpResponseMessage Res = client.GetAsync("api/v1/users/1").Result;
 
-                if (Res.IsSuccessStatusCode)
-                {
-                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
-                    EmpInfo = JsonConvert.DeserializeObject<user>(EmpResponse);
-                }
-                return View(EmpInfo);
-            }
+            //    if (Res.IsSuccessStatusCode)
+            //    {
+            //        var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+            //        EmpInfo = JsonConvert.DeserializeObject<user>(EmpResponse);
+            //    }
+            //    return View(EmpInfo);
+            //}
+            return View();
         }
     }
 }
